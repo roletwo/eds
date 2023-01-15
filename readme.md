9ac9e5a3f87e808cf9b0e9aeeb23fddbb71f9a92
kev <thehailang@gmail.com>
2023-01-16 06:42:42 +0800

    :Sun Jan 15 22:42:42 UTC 2023

--- readme.md
+++ readme.md
@@ -4,10 +4,11 @@
 eds <command>
 
 Commands:
-  eds calc_min <share> <member> <ratio>  Calculate minimal member share
-  eds cut <share> <member> <ratio>       Calculate all member shares as a list
-  eds poll_cut <share> <member> <ratio>  Calculate all member shares as a list
-                                         from a poll list
+  cli.ts poll_cut <share> <member> <ratio>  Calculate all member shares as a
+                                            list from a poll list
+  cli.ts cut <share> <member> <ratio>       Calculate all member shares as a
+                                            list
+  cli.ts calc_min <share> <member> <ratio>  Calculate minimal member share
 
 Options:
   --help     Show help                                                 [boolean]
--- src/cli/main.ts
+++ src/cli/main.ts
@@ -5,94 +5,6 @@ import { n } from '../lib/utility/math';
 
 export const cli = yargs
   .command({
-    command: 'calc_min <share> <member> <ratio>',
-    describe: 'Calculate minimal member share',
-    builder(argv: Argv<any>) {
-      return argv
-        .options({
-          dp: {
-            type: 'number',
-            desc: 'Decimal places to keep in no-rounding mode',
-          },
-        })
-        .positional('share', {
-          describe: 'Total shares',
-          type: 'number',
-        })
-        .positional('member', {
-          describe: 'Total members to split shares',
-          type: 'number',
-        })
-        .positional('ratio', {
-          describe: "How many times a member's share is greater than next member's",
-          type: 'number',
-        });
-    },
-    handler({ share, member, ratio, no_rounding, dp }: any) {
-      const r = calc_min({ share, member, ratio, dp });
-      console.info(r);
-    },
-  })
-  .command({
-    command: 'cut <share> <member> <ratio>',
-    describe: 'Calculate all member shares as a list',
-    builder(argv: Argv<any>) {
-      return argv
-        .options({
-          dp: {
-            type: 'number',
-            desc: 'Decimal places to keep in no-rounding mode',
-          },
-          pretty: {
-            type: 'boolean',
-            desc: 'Pretty print',
-          },
-          verbose: {
-            type: 'boolean',
-          },
-        })
-        .positional('share', {
-          describe: 'Total shares',
-          type: 'number',
-        })
-        .positional('member', {
-          describe: 'Total members to split shares',
-          type: 'number',
-        })
-        .positional('ratio', {
-          describe: "How many times a member's share is greater than next member's",
-          type: 'number',
-        });
-    },
-    handler({ share, member, ratio, no_rounding, dp, pretty, verbose }: any) {
-      const list = cut({ share, member, ratio, dp });
-      const sum = list.reduce((a, b) => a.add(b), n(0)).toDP(dp);
-      const crumb = n(share).minus(sum).toDP(dp);
-      if (pretty) {
-        const table_info = new Table({
-          head: ['Sum', 'Crumb'],
-          rows: [[sum.toString(), crumb.toString()]],
-        });
-
-        const table_list = new Table({
-          head: ['Ranking', 'Shares'],
-          rows: list.map((it, i) => [(i + 1).toString(), it.toString()]),
-        });
-
-        if (verbose) {
-          console.info('Info');
-          console.info(table_info.toString());
-          console.info('List');
-          console.info(table_list.toString());
-        } else {
-          console.info(table_list.toString());
-        }
-      } else {
-        console.info(JSON.stringify({ list, sum, crumb }, null, 2));
-      }
-    },
-  })
-  .command({
     command: 'poll_cut <share> <member> <ratio>',
     describe: 'Calculate all member shares as a list from a poll list',
     builder(argv: Argv<any>) {
@@ -170,6 +82,94 @@ export const cli = yargs
       } else {
         console.info(JSON.stringify({ list, sum, crumb }, null, 2));
       }
+    },
+  })
+  .command({
+    command: 'cut <share> <member> <ratio>',
+    describe: 'Calculate all member shares as a list',
+    builder(argv: Argv<any>) {
+      return argv
+        .options({
+          dp: {
+            type: 'number',
+            desc: 'Decimal places to keep in no-rounding mode',
+          },
+          pretty: {
+            type: 'boolean',
+            desc: 'Pretty print',
+          },
+          verbose: {
+            type: 'boolean',
+          },
+        })
+        .positional('share', {
+          describe: 'Total shares',
+          type: 'number',
+        })
+        .positional('member', {
+          describe: 'Total members to split shares',
+          type: 'number',
+        })
+        .positional('ratio', {
+          describe: "How many times a member's share is greater than next member's",
+          type: 'number',
+        });
+    },
+    handler({ share, member, ratio, no_rounding, dp, pretty, verbose }: any) {
+      const list = cut({ share, member, ratio, dp });
+      const sum = list.reduce((a, b) => a.add(b), n(0)).toDP(dp);
+      const crumb = n(share).minus(sum).toDP(dp);
+      if (pretty) {
+        const table_info = new Table({
+          head: ['Sum', 'Crumb'],
+          rows: [[sum.toString(), crumb.toString()]],
+        });
+
+        const table_list = new Table({
+          head: ['Ranking', 'Shares'],
+          rows: list.map((it, i) => [(i + 1).toString(), it.toString()]),
+        });
+
+        if (verbose) {
+          console.info('Info');
+          console.info(table_info.toString());
+          console.info('List');
+          console.info(table_list.toString());
+        } else {
+          console.info(table_list.toString());
+        }
+      } else {
+        console.info(JSON.stringify({ list, sum, crumb }, null, 2));
+      }
+    },
+  })
+  .command({
+    command: 'calc_min <share> <member> <ratio>',
+    describe: 'Calculate minimal member share',
+    builder(argv: Argv<any>) {
+      return argv
+        .options({
+          dp: {
+            type: 'number',
+            desc: 'Decimal places to keep in no-rounding mode',
+          },
+        })
+        .positional('share', {
+          describe: 'Total shares',
+          type: 'number',
+        })
+        .positional('member', {
+          describe: 'Total members to split shares',
+          type: 'number',
+        })
+        .positional('ratio', {
+          describe: "How many times a member's share is greater than next member's",
+          type: 'number',
+        });
+    },
+    handler({ share, member, ratio, no_rounding, dp }: any) {
+      const r = calc_min({ share, member, ratio, dp });
+      console.info(r);
     },
   })
   .demandCommand();
